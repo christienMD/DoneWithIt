d12f55cece7f1e85a7b5dccbd22fec6a4a107225
Md Christien <mdchristien12@gmail.com>
2024-03-15 13:43:16 +0100

    build a reusable text input component

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Screen from "./app/components/Screen";
+import { TextInput } from "react-native";
+import AppTextInput from "./app/components/AppTextInput";
 
 export default function App() {
   return (
     <Screen>
-
+      
     </Screen>
   );
 }
