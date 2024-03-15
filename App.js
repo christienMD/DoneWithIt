e08b567c7f7f577cae777f12a496e1dcb3953f37
Md Christien <mdchristien12@gmail.com>
2024-03-15 12:37:15 +0100

    Build the Listings Screen

--- App.js
+++ App.js
@@ -1,14 +1,10 @@
 import React from "react";
-import MessagesScreen from "./app/screens/MessagesScreen";
 import Screen from "./app/components/Screen";
-import Icon from "./app/components/Icon";
-import ListItem from "./app/components/ListItem";
-import AccountScreen from "./app/screens/AccountScreen";
 
 export default function App() {
   return (
     <Screen>
-      <AccountScreen />
+
     </Screen>
   );
 }
